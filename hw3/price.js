d0a6a66e4b6f7ab0ea554ba651ab67f387efe8f4
elliay <91764038+elliay@users.noreply.github.com>
2023-02-15 15:50:27 -0500

    fixed hw 3

--- hw3/price.js
+++ hw3/price.js
@@ -1,8 +1,3 @@
-function glazingChange(element){
-    //get value of selected glazing option
-    const priceChange = element.value;
-
-}
 
 let allGlazingChanges = [
     {text: 'Keep original', change: 0},
@@ -10,14 +5,24 @@ let allGlazingChanges = [
     {text: 'Vanilla milk', change: 0.50},
     {text: 'Double chocolate', change: 1.50},
   ];
+
+let allSizeChanges = [
+    {text: '1', change: 1},
+    {text: '3', change: 3},
+    {text: '6', change: 5},
+    {text: '12', change: 10},
+  ];
+
+let glazeIndex = 0;
+let sizeIndex = 0;
   
   /**
-   * Updates the UI to display a particular car's info.
-   * @param glazeChange A car object containing a model and a description.
+   * Updates the UI to display a particular 's info.
+   * @param glazeChange A change object containing a name and a change amount.
    */
-  function displayNewPrice(glazeChange) {
-    let price = document.querySelector('#details-price');
-    let newprice = glazeChange.change + 2.49;
+  function displayNewPrice(glazeChange, sizeChange) {
+    let price = document.querySelector('.details-price');
+    let newprice = ((glazeChange.change + 2.49) * sizeChange.change).toFixed(2);
     console.log("$" + newprice);
     price.innerText = "$" + newprice;
   }
@@ -29,33 +34,50 @@ let allGlazingChanges = [
     console.log('You selected ' + element.value);
   
     // We need to convert the string value to an integer
-    let glazeIndex = parseInt(element.value);
+    glazeIndex = parseInt(element.value);
   
     // Now retrieve the object at the index specified by the select's value
-    let glazeChangetoMake = allGlazingChanges[glazeIndex];
+    glazeChangetoMake = allGlazingChanges[glazeIndex];
   
     // Update the UI
-    displayNewPrice(glazeChangetoMake);
+    displayNewPrice(glazeChangetoMake, allSizeChanges[sizeIndex]);
+  }
+
+  function sizeChange(element) {
+    // In this function, `this` corresponds to the select
+    // element. So `this.value` will contain the value of the
+    // selected option as a string.
+    console.log('You selected ' + element.value);
+  
+    // We need to convert the string value to an integer
+    sizeIndex = parseInt(element.value);
+  
+    // Now retrieve the object at the index specified by the select's value
+    sizeChangetoMake = allSizeChanges[sizeIndex];
+  
+    // Update the UI
+    displayNewPrice(allGlazingChanges[glazeIndex], sizeChangetoMake);
   }
   
   // When the page loads, find the select element.
-  let selectElement = document.querySelector('#glazingOptions');
-  
-  // Give it a listener for the 'change' event, which is a function that will run
-  // when the selected option changes. You could also do this by setting the
-//   // onchange property of selectElement, e.g. selectElement.onchange = ...
-//   selectElement.addEventListener('change', onSelectValueChange);
+  let selectGlazeElement = document.querySelector('#glazingOptions');
+  let selectPackElement = document.querySelector('#sizeOptions')
   
 
 for (var i = 0; i < allGlazingChanges.length; i++){
-    console.log("asdfasdfsdf");
     let option = document.createElement("option");
     option.setAttribute('value', i);
-
     let optionText = document.createTextNode(allGlazingChanges[i].text);
     option.appendChild(optionText);
-    // selectElement.append("<option value='" + i + "'>" + allGlazingChanges[i].text + "</option>");
-    selectElement.append(option);
+    selectGlazeElement.append(option);
+}
+
+for (var i = 0; i < allSizeChanges.length; i++){
+    let option = document.createElement("option");
+    option.setAttribute('value', i);
+    let optionText = document.createTextNode(allSizeChanges[i].text);
+    option.appendChild(optionText);
+    selectPackElement.append(option);
 }
 
 displayNewPrice(allGlazingChanges[0]);
