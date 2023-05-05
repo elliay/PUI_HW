a269a53da2a9dc656ba50574b39a7af120c7aa04
elliay <91764038+elliay@users.noreply.github.com>
2023-05-04 23:09:25 -0400

    changed limit on num tracks

--- final/js/script.js
+++ final/js/script.js
@@ -69,7 +69,7 @@ async function getAccessToken(clientId, code) {
 }
 
 async function fetchProfile(token) {
-    const result = await fetch("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50&offset=0", {
+    const result = await fetch("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=12&offset=0", {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
     });
 
